5704e3c974b2a85f92ed57bce864967e6b3a8933
A. Flaxman <abie@alum.mit.edu>
2009-03-03 14:57:05 -0800

    rename templates dir appropriately

--- settings.py
+++ settings.py
@@ -69,10 +69,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/abie/gbd/views',
-    '/home/a/gbd/views',
-    '/home/abie/gbd/dismod3/views',
-    '/home/a/gbd/dismod3/views',
+    '/home/abie/gbd/templates',
+    '/home/a/gbd/templates',
 
 )
 
