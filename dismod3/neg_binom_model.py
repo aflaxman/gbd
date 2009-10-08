01cbad8f9e96cf706c4b737cebd600a301c0c650
Abraham Flaxman <abie@alum.mit.edu>
2009-10-07 19:01:43 -0700

    make emp prior take reasonable # of samples again

--- dismod3/neg_binom_model.py
+++ dismod3/neg_binom_model.py
@@ -64,8 +64,7 @@ def fit_emp_prior(dm, param_type):
     dm.mcmc = mc.MCMC(dm.vars)
     try:
         #dm.map.fit(method='fmin_powell', iterlim=500, tol=.00001, verbose=1)
-        #dm.mcmc.sample(1000)
-        dm.mcmc.sample(100)
+        dm.mcmc.sample(1000)
     except KeyboardInterrupt:
         print 'User halted optimization routine before optimal value found'
 
