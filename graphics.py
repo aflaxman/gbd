e0521b18a8b2165de350c036b99da35909e80a63
Abraham Flaxman <abie@uw.edu>
2010-09-08 13:35:37 -0700

    remove hard-coded axis stuff in graphics routines

--- graphics.py
+++ graphics.py
@@ -30,7 +30,7 @@ attributes:
 
 import pylab as pl
 
-def plot_prediction_over_time(country, data, predicted, color='blue', alpha=.5, more_data=None):
+def plot_prediction_over_time(country, data, predicted, pred_stats=None, color='blue', alpha=.5, more_data=None):
     """ Plot the predicted values for a specific country as a function of time
 
     Parameters
@@ -39,7 +39,8 @@ def plot_prediction_over_time(country, data, predicted, color='blue', alpha=.5, 
     data : data rec
     predicted : pymc trace
     """
-    pred_stats = predicted.stats(batches=1)
+    if pred_stats == None:
+        pred_stats = predicted.stats(batches=1)
     i_c = [i for i in range(len(data)) if data.country[i] == country]
     T = len(i_c)
     n = len(predicted.trace())
@@ -74,13 +75,13 @@ def plot_all_predictions_over_time(data, predicted, color='blue', alpha=.5, more
     data : data rec
     predicted : pymc trace
     """
-    # memorize stats to speed country-specific plots (HACK)
-    stats_func = predicted.stats  # save stats function
-    stats_val = stats_func(batches=1)  # save results of calling stats function
-    predicted.stats = lambda batches=1: stats_val  # replace function that does calculation with function that returns memorized results
+    # memorize stats to speed country-specific plots
+    pred_stats = predicted.stats(batches=1)
 
-    y_min = round(min(predicted.stats()['95% HPD interval'][:,0]), -1)
-    y_max = round(max(predicted.stats()['95% HPD interval'][:,1]), -1)
+    x_min = min(data.year)
+    x_max = max(data.year)
+    y_min = round(min(pred_stats['95% HPD interval'][:,0]), -1)
+    y_max = round(max(pred_stats['95% HPD interval'][:,1]), -1)
     max_countries = 4   # FIXME: don't hard-code constant 4
     regions = sorted(set(data.region))[:4] # FIXME: don't hard-code constant 4
     for ii, region in enumerate(regions):
@@ -95,11 +96,11 @@ def plot_all_predictions_over_time(data, predicted, color='blue', alpha=.5, more
         for jj, country in enumerate(countries):
             # plot and label the cell
             pl.subplot(len(regions), max_countries, ii*max_countries + jj + 1)
-            plot_prediction_over_time(country, data, predicted, color=color, alpha=alpha, more_data=more_data)
+            plot_prediction_over_time(country, data, predicted, pred_stats=pred_stats, color=color, alpha=alpha, more_data=more_data)
             pl.title('\n\n'+country, va='top')
 
             # set the axis
-            pl.axis([1988, 2007, 1.2*y_min, 1.2*y_max]) # FIXME: don't hard-code constants
+            pl.axis([x_min-2, x_max+2, 1.2*y_min, 1.2*y_max])
             if jj > 0:
                 pl.yticks([])
             else:
@@ -107,14 +108,10 @@ def plot_all_predictions_over_time(data, predicted, color='blue', alpha=.5, more
             if ii < len(regions)-1:
                 pl.xticks([])
             else:
-                pl.xticks([1990, 1995, 2000, 2005]) # FIXME: don't hard-code constants
+                pl.xticks(range(x_min, x_max, 5))
 
     # set the border width correctly
     pl.subplots_adjust(left=.05, right=.99, bottom=.05, top=.99, wspace=0, hspace=0)
-
-    # undo memorization hack
-    predicted.stats = stats_func
-
 
 def test(data, mc_dict):
     """ Test plots for all mcmc results in the mc_dict"""
