d492cc70775976e03481ec9cfccbb32d74ece982
A. Flaxman <abie@alum.mit.edu>
2010-12-21 09:04:32 -0800

    more output from sim study

--- simulation_study.py
+++ simulation_study.py
@@ -105,13 +105,14 @@ if __name__ == '__main__':
     if len(args) == 0:
         # print summary results
         Y = []
-        print '%25s\t|\t%s\t|\t%s\t|\t%s\t\\\\' % ('fname', 'n', 'MARE (%)', 'Coverage (%)')
+        print '%s\t|\t%s\t|\t%s\t|\t%s\t|\t%s\t\\\\' % ('nrows', 'cv', 'nreps', 'MARE (%)', 'Coverage (%)')
         import glob
         for fname in sorted(glob.glob('score*.txt')):
             X = csv2rec(fname, names=['mare', 'coverage'])
-            print '%25s\t|\t%d\t|\t%2.2f\t|\t%.2f\t\\\\' % (fname, len(X), mean(X.mare), mean(X.coverage)*100)
             n = float(fname.split('_')[1])
             cv = float(fname.split('_')[2][:-4])
+
+            print '%d\t|\t%d\t|\t%d\t|\t%2.2f\t|\t%.2f\t\\\\' % (n, cv, len(X), mean(X.mare), mean(X.coverage)*100)
             Y.append([n, cv, len(X), X.mare.mean(), X.mare.std(), X.coverage.mean()*100, X.coverage.std()*100])
         f = open('/home/j/Project/Models/dm_scores.csv', 'w')
         import csv
