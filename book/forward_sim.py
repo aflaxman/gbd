9711f59a581fcf54b0d93cd7f2f4c1025e859fae
Abraham Flaxman <abie@alum.mit.edu>
2011-07-25 13:03:44 -0700

    initial commit of code to generate examples for dismod book

---
+++ book/forward_sim.py
@@ -0,0 +1,14 @@
+""" Generate consistent prevalence rates as function of age from
+incidence, remission, and excess-mortality"""
+
+### @export ex1
+
+import sys
+sys.path.append("../..")
+
+import dismod3
+
+print 2+2
+
+# generate figures
+
