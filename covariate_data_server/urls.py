77f1508787a581dd543ad3472852820da4bbcda2
Abraham Flaxman <abie@alum.mit.edu>
2011-08-02 12:01:22 -0700

    allow spaces in urls to view covariate values

--- covariate_data_server/urls.py
+++ covariate_data_server/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('gbd.covariate_data_server.views',
     (r'summary/(\d+)$', 'covariate_type_show'),
-    (r'show/([\w-]+)\+(\w+)\+(\w+).(\w+)$', 'covariate_show'),
+    (r'show/([ \w-]+)\+(\w+)\+(\w+).(\w+)$', 'covariate_show'),
     (r'upload/$', 'covariate_upload'),
 )
