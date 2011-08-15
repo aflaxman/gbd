316a80400d561f3c34b7af56af4b68c4caf94728
Abraham Flaxman <abie@alum.mit.edu>
2011-08-15 13:39:21 -0700

    update age-pattern code

--- book/age_patterns.py
+++ book/age_patterns.py
@@ -15,39 +15,42 @@ reload(book_graphics)
 
 
 ### @export 'models-of-varying-smoothness'
+data = pl.array([[10., .1, .025],
+                 [50., .25, .025]])
+smoothness=200 # very smooth
+smoothness=100 # moderately smooth
+smoothness=50 # slightly smooth
+
 ## setup lognormal prior on intercept
-gamma = mc.Normal('gamma', 0., 2.**-2, value=0.)
+gamma = mc.Normal('gamma', 0.,
+                  2.**-2,
+                  value=pl.log(data[:,1].mean()))
 mu = mc.Lambda('mu', lambda gamma=gamma: pl.exp(gamma))
 
 ## setup Gaussian Process prior on age pattern
-mesh = pl.arange(0,101,1) #dismod3.settings.gbd_ages
-M = mc.gp.Mean(lambda x: pl.zeros(len(x)))
+mesh = pl.arange(0,101,1)
+@mc.deterministic
+def M(mu=mu):
+    return mc.gp.Mean(lambda x: mu*pl.ones(len(x)))
 C = mc.gp.FullRankCovariance(mc.gp.matern.euclidean,
-                             amp=.1,
-                             scale=20., 
+                             amp=data[:,1].max(),
+                             scale=smoothness,
                              diff_degree=2)
 sm = mc.gp.GPSubmodel('sm', M, C, mesh,
-                      init_vals=pl.zeros_like(mesh))
-
-## combine to get age-specific mean of rate
-@mc.deterministic
-def f(sm=sm, mu=mu):
-    return mu + sm.f(pl.arange(101))
+                      init_vals=mu.value*pl.ones_like(mesh))
 
 ## condition on rate being positive
 @mc.potential
-def positive(f=f):
+def positive(f=sm.f_eval):
     if pl.any(f < 0.):
         return -pl.inf
     else:
         return 0.
 
 ## likelihood of observed data, using normal model for simplicity
-data = pl.array([[ 0., .1, .005],
-                 [50., .5, .005]])
 @mc.deterministic
-def data_expected(f=f):
-    return [f[data[0,0]], f[data[1,0]]],  # TODO: make this line more elegant
+def data_expected(mu=mu, sm=sm):
+    return sm.f(data[:,0])
 @mc.observed
 def data_obs(data_expected=data_expected, value=data):
     return mc.normal_like(value[:,1],
@@ -56,12 +59,12 @@ def data_obs(data_expected=data_expected, value=data):
 
 ## generate good initial values with MAP fit
 mc.MAP([gamma, data_obs]).fit(method='fmin_powell', verbose=1)
+mc.MAP([sm.f_eval, data_obs]).fit(method='fmin_powell', verbose=1)
+mc.MAP([gamma, data_obs]).fit(method='fmin_powell', verbose=1)
 
 ## combine all PyMC nodes in to an MCMC object
-mcmc = mc.MCMC([sm, gamma, mu, f, positive, data_expected, data_obs])
-
-#mcmc.use_step_method(mc.gp.GPParentAdaptiveMetropolis, gamma)
-#mcmc.use_step_method(mc.AdaptiveMetropolis, [gamma, sm.f_eval])
+mcmc = mc.MCMC([gamma, mu, M, sm, positive, data_expected, data_obs])
+mcmc.use_step_method(mc.gp.GPParentAdaptiveMetropolis, gamma)
 mcmc.sample(10000, 5000, 10)
 
 ### @export 'plot-varying-smoothing'
@@ -71,15 +74,24 @@ mc.Matplot.plot(mu)
 pl.figure()
 ages = pl.arange(0,101,1)
 
-for f_n in f.trace():
+for i, f_n in enumerate(sm.f_eval.trace()):
     pl.plot(ages, f_n,
          '-', color='grey',
             zorder=-1)
-pl.plot(ages, f.stats()['quantiles'][50],
-        'k-', linewidth=3)
+    if i % (len(sm.f_eval.trace())/4) == 0:
+        pl.plot(ages, f_n,
+                '-', linewidth=2,
+                color=pl.cm.spectral(.05+float(i)/len(sm.f_eval.trace())),
+                zorder=0)
+        
+pl.plot(ages, sm.f_eval.stats()['quantiles'][50],
+        'k-', linewidth=3, label='Median')
+pl.plot(ages, sm.f_eval.stats()['mean'],
+        'k--', linewidth=3, label='Mean')
 pl.errorbar(data[:,0], data[:,1], yerr=data[:,2]*1.96,
             fmt='gs',
-            mec='white', mew=1, ms=10)
+            mec='white', mew=1, ms=5)
+pl.legend()
 
 pl.savefig('smoothness_priors.pdf')
 
