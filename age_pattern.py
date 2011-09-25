17b38874f0c1faf573341fd7177010c27e89e5f5
Abraham Flaxman <abie@alum.mit.edu>
2011-09-25 02:24:24 -0700

    most of an pcgp age pattern model

---
+++ age_pattern.py
@@ -0,0 +1,35 @@
+""" Several rate models"""
+
+import pylab as pl
+import pymc as mc
+
+
+def pcgp(name, gamma_bar, knots, rho):
+    """ Generate PyMC objects for a piecewise constant Gaussian process (PCGP) model
+
+    Parameters
+    ----------
+    name : str
+    gamma_bar : pymc.Node, expected values of PCGP
+    knots : array, locations of the discontinuities in the piecewise constant function
+    rho : pymc.Node, smoothness parameter for Matern covariance of GP
+
+    Results
+    -------
+    Returns dict of PyMC objects, including 'gamma' and 'mu'
+    the observed stochastic likelihood and data predicted stochastic
+    """
+
+    C_func = mc.gp.FullRankCovariance(mc.gp.matern.euclidean, amp=1., scale=rho, diff_degree=2)
+    C_chol = pl.cholesky(C_func(knots, knots))  # FIXME: choose these points dynamically and appropriately
+    gamma = mc.MvNormalChol('gamma_%s'%name, mu=pl.zeros_like(knots), sig=C_chol, value=pl.zeros_like(knots))
+    #gamma = mc.Uninformative('gamma_%s'%name, value=pl.zeros_like(knots))
+
+    import scipy.interpolate
+    all_ages = pl.arange(100)
+    @mc.deterministic(name='mu_%s'%name)
+    def mu(gamma_bar=gamma_bar, gamma=gamma, knots=knots):
+        mu = scipy.interpolate.interp1d(knots, pl.exp(gamma_bar + gamma), 'zero', bounds_error=False, fill_value=0.)
+        return mu(all_ages)
+
+    return dict(gamma=gamma, mu=mu)
---
+++ tests/test_age_pattern.py
@@ -0,0 +1,60 @@
+""" Test Age Pattern Model
+
+These tests are use randomized computation, so they might fail
+occasionally due to stochastic variation
+"""
+
+# matplotlib will open windows during testing unless you do the following
+import matplotlib
+matplotlib.use("AGG")
+
+# add to path, to make importing possible
+import sys
+sys.path += ['.', '..']
+
+import pylab as pl
+import pymc as mc
+
+import data
+import rate_model
+import age_pattern
+reload(age_pattern)
+
+def test_age_pattern_model_sim():
+    # simulate normal data
+    a = pl.arange(0, 100, 5)
+    pi_true = .0001 * (a * (100. - a) + 100.)
+    sigma_true = .025
+
+    p = mc.rnormal(pi_true, 1./sigma_true**2.)
+
+    # create model and priors
+    vars = dict(gamma_bar=mc.Uninformative('gamma_bar', value=0.))
+
+    vars.update(age_pattern.pcgp('test', vars['gamma_bar'], knots=pl.arange(0,101,5), rho=25.))
+
+    vars['pi'] = mc.Lambda('pi', lambda mu=vars['mu'], a=a: mu[a])
+    vars.update(rate_model.normal_model('test', vars['pi'], 0., p, sigma_true))
+
+    # fit model
+    mc.MAP(vars).fit(method='fmin_powell', verbose=1)
+    m = mc.MCMC(vars)
+    m.use_step_method(mc.AdaptiveMetropolis, [m.gamma_bar, m.gamma])
+    m.sample(20000, 10000, 10)
+
+    # plot results
+    pl.plot(pl.arange(100), m.mu.stats()['95% HPD interval'], 'k', linestyle='steps-post:')
+    pl.plot(pl.arange(100), m.mu.stats()['mean'], 'k-', drawstyle='steps-post')
+    pl.plot(a, pi_true, 'g-')
+    pl.plot(a, p, 'ro')
+
+    # compare estimate to ground truth (skip endpoints, because they are extra hard to get right)
+    assert pl.allclose(m.pi.stats()['mean'][1:-1], pi_true[1:-1], rtol=.2)
+    lb, ub = m.pi.stats()['95% HPD interval'].T
+    assert pl.mean((lb <= pi_true)[1:-1] & (pi_true <= ub)[1:-1]) > .75
+
+
+if __name__ == '__main__':
+    import nose
+    nose.runmodule()
+    
