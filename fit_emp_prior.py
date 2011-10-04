c0ab5d3948a10cb4d1b99d699af1219f55a752d2
Abraham Flaxman <abie@alum.mit.edu>
2011-10-03 22:23:26 -0700

    make fit_posterior and fit_emp_prior work with new model implementation

--- fit_emp_prior.py
+++ fit_emp_prior.py
@@ -52,6 +52,8 @@ def fit_emp_prior(id, param_type, map_only=False):
     for col in model.input_data.columns:
         if col.startswith('x_'):
             model.input_data[col] = model.input_data[col].fillna(0.)
+    # also fill all covariates missing in output template with zeros
+    model.output_template = model.output_template.fillna(0)
 
     t = {'incidence': 'i', 'prevalence': 'p', 'remission': 'r', 'excess-mortality': 'f'}[param_type]
     data = model.get_data(t)
@@ -63,21 +65,29 @@ def fit_emp_prior(id, param_type, map_only=False):
     vars = data_model.data_model('prior', model, t,
                                  root_area='all', root_sex='total', root_year='all',
                                  mu_age=None, mu_age_parent=None, rate_type=(t == 'rr') and 'log_normal' or 'neg_binom')
+    if map_only:
+        fit_model.fit_data_model(vars, iter=101, burn=0, thin=1, tune_interval=100)
+    else:
+        fit_model.fit_data_model(vars, iter=10050, burn=5000, thin=50, tune_interval=100)
 
-    fit_model.fit_data_model(vars, iter=10001, burn=1000, thin=9, tune_interval=100)
 
-    for a in dismod3.settings.gbd_regions:
-        print 'generating empirical prior for %s', a
+    graphics.plot_one_type(model, vars, {}, t)
+    for a in model.hierarchy['all']:
+        print 'generating empirical prior for %s' % a
         for s in dismod3.settings.gbd_sexes:
             for y in dismod3.settings.gbd_years:
+                key = dismod3.utils.gbd_key_for(param_type, a, y, s)
                 emp_priors = covariate_model.predict_for(model.output_template, model.hierarchy,
-                                                            'all', 'total', 'all',
-                                                            dismod3.utils.clean(a),
-                                                            dismod3.utils.clean(s),
-                                                            int(y), vars)
-                dm.set_mcmc('emp_prior_mean', dismod3.utils.gbd_key_for(param_type, a, y, s), emp_priors.mean(0))
+                                                         'all', 'total', 'all',
+                                                         a, dismod3.utils.clean(s), int(y),
+                                                         vars)
+                n = len(emp_priors)
+                emp_priors.sort(axis=0)
+                dm.set_mcmc('emp_prior_mean', key, emp_priors.mean(0))
+                dm.set_mcmc('emp_prior_std', key, emp_priors.std(0))
     
-    graphics.plot_one_type(model, vars, emp_priors, t)
+                pl.plot(model.parameters['ages'], dm.get_mcmc('emp_prior_mean', key), 'r-')
+
     pl.savefig(dir + '/prior-%s.png'%param_type)
 
     graphics.plot_one_ppc(vars, t)
