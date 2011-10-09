9968f7b725ca8d594de3838c3a471db9ce4a6aa9
Abraham Flaxman <abie@alum.mit.edu>
2011-10-09 15:19:55 -0700

    add work-around for pymc issue that is preventing using AM step-method for gamma

--- age_pattern.py
+++ age_pattern.py
@@ -21,9 +21,13 @@ def pcgp(name, ages, knots, sigma):
     """
     gamma = [mc.Normal('gamma_%s_%d'%(name,k), 0., 10.**-2, value=0) for k in knots]
 
+    # TODO: fix AdaptiveMetropolis so that this is not necessary
+    flat_gamma = mc.Lambda('flat_gamma_%s'%name, lambda gamma=gamma: pl.array([x for x in pl.flatten(gamma)]))
+
+
     import scipy.interpolate
     @mc.deterministic(name='mu_age_%s'%name)
-    def mu_age(gamma=gamma, knots=knots, ages=ages):
+    def mu_age(gamma=flat_gamma, knots=knots, ages=ages):
         mu = scipy.interpolate.interp1d(knots, pl.exp(gamma), 'linear', bounds_error=False, fill_value=0.)
         return mu(ages)
 
@@ -32,7 +36,7 @@ def pcgp(name, ages, knots, sigma):
     if sigma > 0.:
         print 'adding smoothing of', sigma
         @mc.potential(name='smooth_mu_%s'%name)
-        def smooth_gamma(gamma=gamma, knots=knots, tau=sigma**-2):
+        def smooth_gamma(gamma=flat_gamma, knots=knots, tau=sigma**-2):
             return mc.normal_like(pl.diff(gamma), 0, tau/pl.diff(knots))
         vars['smooth_gamma'] = smooth_gamma
 
