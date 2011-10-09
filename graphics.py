a897161dc3466955958a74a58b82fd3daad4c727
Abraham Flaxman <abie@alum.mit.edu>
2011-10-09 13:44:13 -0700

    bug fix for delta ui in plot_one_model

--- graphics.py
+++ graphics.py
@@ -89,7 +89,7 @@ def plot_one_type(model, vars, emp_priors, t):
     if 'delta' in vars:
         stats = vars['delta'].stats()
         if stats:
-            delta = '%.2f (%.2f, %.2f)' % (stats['mean'], stats['95% HPD interval'][0], stats['95% HPD interval'][0])
+            delta = '%.2f (%.2f, %.2f)' % (stats['mean'], stats['95% HPD interval'][0], stats['95% HPD interval'][1])
         else:
             delta = '%.2f' % vars['delta'].value
 
