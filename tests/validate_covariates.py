323422e9c5b30805ee8db21395ee5f90ee81509a
Abraham Flaxman <abie@alum.mit.edu>
2011-10-24 20:23:13 -0700

    changes to input and output from validation

--- tests/validate_covariates.py
+++ tests/validate_covariates.py
@@ -85,13 +85,21 @@ def validate_covariate_model_re():
     N = 100
     delta_true = .5
 
-
-    ## generate simulation data
     import dismod3
     import simplejson as json
     model = data.ModelData.from_gbd_jsons(json.loads(dismod3.disease_json.DiseaseJson().to_json()))
     model.parameters['p']['parameter_age_mesh'] = [0, 100]
 
+    area_list = []
+    for sr in model.hierarchy.successors('all')[:3]:
+        area_list.append(sr)
+        for r in model.hierarchy.successors(sr):
+            area_list.append(r)
+            area_list += model.hierarchy.successors(r)[:5]
+    area_list = pl.array(area_list)
+
+
+    ## generate simulation data
     model.input_data = pandas.DataFrame(index=range(N))
     model.input_data['age_start'] = 0
     model.input_data['age_end'] = 0
@@ -104,56 +112,73 @@ def validate_covariate_model_re():
     model.input_data['lower_ci'] = pl.nan
 
 
+
     # 1. choose pi^true
-    pi_true = mc.runiform(.0001, .1)
+    pi_true = .01
     a = pl.arange(0, 100, 1)
     pi_age_true = pi_true * pl.ones_like(a)
 
 
     # 2. choose sigma^true
-    sigma_true = mc.runiform(.05, .5, 4)
+    sigma_true = [.05, .3, .2, .1, .05]
 
 
     # 3. choose alpha^true
     alpha = dict(all=0.)
     sum_sr = 0.
+    last_sr = -1
     for sr in model.hierarchy['all']:
+        if sr not in area_list:
+            continue
+
         sum_r = 0.
+        last_r = -1
         for r in model.hierarchy[sr]:
+            if r not in area_list:
+                continue
+
             sum_c = 0.
+            last_c = -1
             for c in model.hierarchy[r]:
+                if c not in area_list:
+                    continue
+
                 alpha[c] = mc.rnormal(0., sigma_true[3]**-2.)
                 sum_c += alpha[c]
-            alpha[c] -= sum_c
+                last_c = c
+            if last_c >= 0:
+                alpha[last_c] -= sum_c
 
             alpha[r] = mc.rnormal(0., sigma_true[2]**-2.)
             sum_r += alpha[r]
-        alpha[r] -= sum_r
+            last_r = r
+        if last_r >= 0:
+            alpha[last_r] -= sum_r
 
         alpha[sr] = mc.rnormal(0., sigma_true[1]**-2.)
         sum_sr += alpha[sr]
-    alpha[sr] -= sum_sr
-
+        last_sr = sr
+    if last_sr >= 0:
+        alpha[last_sr] -= sum_sr
 
     # 4. choose observed prevalence values
     model.input_data['effective_sample_size'] = mc.runiform(100, 10000, N)
 
-    area_list = pl.array(nx.traversal.bfs_tree(model.hierarchy, 'australasia').nodes() + nx.traversal.bfs_tree(model.hierarchy, 'north_america_high_income').nodes())
     model.input_data['area'] = area_list[mc.rcategorical(pl.ones(len(area_list)) / float(len(area_list)), N)]
 
-    model.input_data['value_true'] = pl.nan
+    model.input_data['true'] = pl.nan
     for i, a in model.input_data['area'].iteritems():
-        model.input_data['value_true'][i] = pi_true * pl.exp(pl.sum([alpha[n] for n in nx.shortest_path(model.hierarchy, 'all', a)]))
+        model.input_data['true'][i] = pi_true * pl.exp(pl.sum([alpha[n] for n in nx.shortest_path(model.hierarchy, 'all', a) if n in alpha]))
 
     n = model.input_data['effective_sample_size']
-    p = model.input_data['value_true']
+    p = model.input_data['true']
     model.input_data['value'] = mc.rnegative_binomial(n*p, delta_true*n*p) / n
 
 
 
     ## Then fit the model and compare the estimates to the truth
     model.vars = {}
-    model.vars['p'] = data_model.data_model('re_validation', model, 'p', 'all', 'total', 'all', None, None, None)
+    model.vars['p'] = data_model.data_model('p', model, 'p', 'all', 'total', 'all', None, None, None)
     model.map, model.mcmc = fit_model.fit_data_model(model.vars['p'], iter=10000, burn=5000, thin=5, tune_interval=100)
 
     graphics.plot_one_ppc(model.vars['p'], 'p')
@@ -161,23 +186,46 @@ def validate_covariate_model_re():
 
     pl.show()
 
+    def metrics(df):
+        df['abs_err'] = df['true'] - df['mu_pred']
+        df['rel_err'] = (df['true'] - df['mu_pred']) / df['true']
+        df['covered?'] = (df['true'] >= df['mu_pred'] - 1.96*df['sigma_pred']) & (df['true'] <= df['mu_pred'] + 1.96*df['sigma_pred'])
+
     model.input_data['mu_pred'] = model.vars['p']['p_pred'].stats()['mean']
     model.input_data['sigma_pred'] = model.vars['p']['p_pred'].stats()['standard deviation']
+    metrics(model.input_data)
 
-    model.re = pandas.DataFrame(index=[n for n in nx.traversal.dfs_preorder_nodes(model.hierarchy)])
-    model.re['true'] = pandas.Series(dict(alpha))
-    model.re['mu_pred'] = pandas.Series([n.stats()['mean'] for n in model.vars['p']['alpha']], index=model.vars['p']['U'].columns)
-    model.re['sigma_pred'] = pandas.Series([n.stats()['standard deviation'] for n in model.vars['p']['alpha']], index=model.vars['p']['U'].columns)
-    model.re['abs_err'] = model.re['true'] - model.re['mu_pred']
-    model.re['rel_err'] = (model.re['true'] - model.re['mu_pred']) / model.re['true']
-    model.re['coverage'] = (model.re['true'] >= model.re['mu_pred'] - 1.96*model.re['sigma_pred']) & (model.re['true'] <= model.re['mu_pred'] + 1.96*model.re['sigma_pred'])
 
-    print 'Random Effects'
-    print model.re.dropna()
+    model.alpha = pandas.DataFrame(index=[n for n in nx.traversal.dfs_preorder_nodes(model.hierarchy)])
+    model.alpha['true'] = pandas.Series(dict(alpha))
+    model.alpha['mu_pred'] = pandas.Series([n.stats()['mean'] for n in model.vars['p']['alpha']], index=model.vars['p']['U'].columns)
+    model.alpha['sigma_pred'] = pandas.Series([n.stats()['standard deviation'] for n in model.vars['p']['alpha']], index=model.vars['p']['U'].columns)
+    metrics(model.alpha)
 
-    model.sigma = pandas.DataFrame(dict(true=sigma_true,
-                                        mu_pred=[n.stats()['mean'] for n in model.vars['p']['sigma_alpha']]))
+    print '\nalpha'
+    print model.alpha.dropna()
+
+
+    model.sigma = pandas.DataFrame(dict(true=sigma_true))
+    model.sigma['mu_pred'] = [n.stats()['mean'] for n in model.vars['p']['sigma_alpha']]
+    model.sigma['sigma_pred']=[n.stats()['standard deviation'] for n in model.vars['p']['sigma_alpha']]
+    metrics(model.sigma)
+
+    print 'sigma_alpha'
     print model.sigma
+
+    model.delta = pandas.DataFrame(dict(true=[delta_true]))
+    model.delta['mu_pred'] = pl.exp(model.vars['p']['eta'].trace().mean())
+    model.delta['sigma_pred'] = pl.exp(model.vars['p']['eta'].trace().std())
+    metrics(model.delta)
+
+    print 'delta'
+    print model.delta
+
+    print '\ndata prediction bias: %.6f MARE: %.3f' % (model.input_data['abs_err'].mean(),
+                                                     pl.median(pl.absolute(model.input_data['rel_err'].dropna())))
+    print 'effect prediction MAE: %.3f, coverage: %.2' % (pl.median(pl.absolute(model.alpha['abs_err'].dropna())),
+                                                          model.alpha['covered?'].mean())
 
     return model
 
