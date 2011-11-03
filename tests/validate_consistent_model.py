90f55525c900bc90cf590dfc29272446363a86be
Abraham Flaxman <abie@alum.mit.edu>
2011-11-03 01:14:58 -0700

    finished consistent model validation code

--- tests/validate_consistent_model.py
+++ tests/validate_consistent_model.py
@@ -19,23 +19,27 @@ import graphics
 import data_simulation
 import pandas
 
+reload(consistent_model)
 reload(data_simulation)
 reload(graphics)
 
 def quadratic(a):
-    return .0001 * (a * (100. - a) + 100.)
+    return 1e-6 * (a * (100. - a) + 100.)
+
+def constant(a):
+    return .2 * pl.ones_like(a)
 
 def validate_consistent_model_sim(N=500, delta_true=.15,
-                                       true=dict(i=quadratic, f=quadratic, r=quadratic)):
+                                       true=dict(i=quadratic, f=constant, r=constant)):
     ## generate simulated data
     model = data_simulation.simple_model(N)
     model.input_data['effective_sample_size'] = 1.
     model.input_data['value'] = 0.
 
-    vars = consistent_model.consistent_model(model, 'all', 'total', 'all', {})
+    sim = consistent_model.consistent_model(model, 'all', 'total', 'all', {})
     for t in 'irf':
-        for i, k_i in enumerate(vars[t]['knots']):
-            vars[t]['gamma'][i].value = pl.log(true[t](k_i))
+        for i, k_i in enumerate(sim[t]['knots']):
+            sim[t]['gamma'][i].value = pl.log(true[t](k_i))
 
     age_start = pl.array(mc.runiform(0, 100, size=N), dtype=int)
     age_end = pl.array(mc.runiform(age_start, 100, size=N), dtype=int)
@@ -50,7 +54,7 @@ def validate_consistent_model_sim(N=500, delta_true=.15,
 
     p = pl.zeros(N)
     for t in types:
-        mu_t = vars[t]['mu_age'].value
+        mu_t = sim[t]['mu_age'].value
         sum_mu_wt = pl.cumsum(mu_t*age_weights)
     
         p_t = (sum_mu_wt[age_end] - sum_mu_wt[age_start]) / (sum_wt[age_end] - sum_wt[age_start])
@@ -73,27 +77,31 @@ def validate_consistent_model_sim(N=500, delta_true=.15,
 
     # coarse knot spacing for fast testing
     for t in types:
-        model.parameters[t]['parameter_age_mesh'] = range(0, 101, 25)
+        model.parameters[t]['parameter_age_mesh'] = range(0, 101, 20)
 
     ## Then fit the model and compare the estimates to the truth
     model.vars = {}
     model.vars = consistent_model.consistent_model(model, 'all', 'total', 'all', {})
-    model.map, model.mcmc = fit_model.fit_consistent_model(model.vars, iter=1000, burn=500, thin=1, tune_interval=100)
+    model.map, model.mcmc = fit_model.fit_consistent_model(model.vars, iter=10000, burn=5000, thin=25, tune_interval=100)
 
-    graphics.plot_fit(model, model.vars, {}, {})
     graphics.plot_convergence_diag(model.vars)
 
-    graphics.plot_one_type(model, model.vars['p'], {}, 'p')
-    pl.plot(a, vars['p']['mu_age'].value, 'r:', label='Truth')
-    pl.legend(fancybox=True, shadow=True, loc='upper left')
+    graphics.plot_fit(model, model.vars, {}, {})
+    for i, t in enumerate('i r f p rr pf'.split()):
+        pl.subplot(2, 3, i+1)
+        pl.plot(a, sim[t]['mu_age'].value, 'w-', label='Truth', linewidth=2)
+        pl.plot(a, sim[t]['mu_age'].value, 'r-', label='Truth', linewidth=1)
+
+    #graphics.plot_one_type(model, model.vars['p'], {}, 'p')
+    #pl.legend(fancybox=True, shadow=True, loc='upper left')
 
     pl.show()
 
     model.input_data['mu_pred'] = 0.
     model.input_data['sigma_pred'] = 0.
     for t in types:
-        model.input_data['mu_pred'][data_type==t] = model.vars[t]['p_pred'].stats()['mean'][data_type==t]
-        model.input_data['sigma_pred'][data_type==t] = model.vars['p']['p_pred'].stats()['standard deviation'][data_type==t]
+        model.input_data['mu_pred'][data_type==t] = model.vars[t]['p_pred'].stats()['mean']
+        model.input_data['sigma_pred'][data_type==t] = model.vars['p']['p_pred'].stats()['standard deviation']
     data_simulation.add_quality_metrics(model.input_data)
 
     model.delta = pandas.DataFrame(dict(true=[delta_true for t in types]))
@@ -108,14 +116,22 @@ def validate_consistent_model_sim(N=500, delta_true=.15,
                                                      pl.median(pl.absolute(model.input_data['rel_err'].dropna())),
                                                                        model.input_data['covered?'].mean())
 
-    #model.mu = pandas.DataFrame(dict(true=,
-    #                                 mu_pred=model.vars['p']['mu_age'].stats()['mean'],
-    #                                 sigma_pred=model.vars['p']['mu_age'].stats()['standard deviation']))
-    #data_simulation.add_quality_metrics(model.mu)
+    model.mu = pandas.DataFrame()
+    for t in types:
+        model.mu = model.mu.append(pandas.DataFrame(dict(true=sim[t]['mu_age'].value,
+                                                         mu_pred=model.vars[t]['mu_age'].stats()['mean'],
+                                                         sigma_pred=model.vars[t]['mu_age'].stats()['standard deviation'])),
+                                   ignore_index=True)
+    data_simulation.add_quality_metrics(model.mu)
+    print '\nparam prediction bias: %.5f, MARE: %.3f, coverage: %.2f' % (model.mu['abs_err'].mean(),
+                                                                         pl.median(pl.absolute(model.mu['rel_err'].dropna())),
+                                                                         model.mu['covered?'].mean())
+    print
+
 
     model.results = dict(param=[], bias=[], mare=[], mae=[], pc=[])
     data_simulation.add_to_results(model, 'delta')
-    #data_simulation.add_to_results(model, 'mu')
+    data_simulation.add_to_results(model, 'mu')
     data_simulation.add_to_results(model, 'input_data')
     model.results = pandas.DataFrame(model.results, columns='param bias mae mare pc'.split())
 
