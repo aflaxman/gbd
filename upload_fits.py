83c71a577b875a6d1ed0bd6ed5e9b91d157c93c7
A. Flaxman <abie@alum.mit.edu>
2011-11-08 19:07:09 -0800

    add a way to compare data observed to predicted

--- upload_fits.py
+++ upload_fits.py
@@ -53,6 +53,36 @@ def upload_fits(id):
     f.write('\n**** JOB DONE AT %s' % time.strftime('%c'))
     f.close()
 
+def merge_data_csvs(id):
+    import glob
+    import pylab as pl
+    import pymc as mc
+    import pandas
+
+    df = pandas.DataFrame()
+
+    dir = dismod3.settings.JOB_WORKING_DIR % id
+    print dir
+    for f in sorted(glob.glob('%s/posterior/data-*.csv'%dir)):
+        print 'merging %s' % f
+        df2 = pandas.read_csv(f, index_col=None)
+        df2.index = df2['index']
+        df = df.drop(set(df.index)&set(df2.index)).append(df2)
+
+    df['residual'] = df['value'] - df['mu_pred']
+    df['scaled_residual'] = df['residual'] / pl.sqrt(df['value'] * (1 - df['value']) / df['effective_sample_size'])
+    #df['scaled_residual'] = df['residual'] * pl.sqrt(df['effective_sample_size'])  # including 
+    df['abs_scaled_residual'] = pl.absolute(df['scaled_residual'])
+
+    d = .005 # TODO: save delta in these files, use negative binomial to calc logp
+    df['logp'] = [mc.negative_binomial_like(x*n, (p+1e-3)*n, d*(p+1e-3)*n) for x,p,n in zip(df['value'], df['mu_pred'], df['effective_sample_size'])]
+    df['logp'][df['data_type'] == 'rr'] = df['scaled_residual'][df['data_type'] == 'rr']
+
+    df = df.sort('logp')
+
+    print df.filter('data_type area age_start age_end year_start sex effective_sample_size value residual logp'.split())[:25]
+    return df
+
 def main():
     import optparse
 
