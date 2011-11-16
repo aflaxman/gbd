56055ba82b2824ceaf381a534d2b9085605de202
A. Flaxman <abie@alum.mit.edu>
2011-11-15 20:39:18 -0800

    make logp tally more robust

--- fit_world.py
+++ fit_world.py
@@ -164,7 +164,10 @@ def fit_world(id, map_only=False):
             dm.vars[t]['data']['abs_residual'] = pl.absolute(dm.vars[t]['data']['residual'])
             if 'delta' in dm.vars[t]:
                 dm.vars[t]['data']['logp'] = [mc.negative_binomial_like(n*p_obs, n*p_pred, n*p_pred*d) for n, p_obs, p_pred, d \
-                                                  in zip(dm.vars[t]['data']['effective_sample_size'], dm.vars[t]['data']['value'], dm.vars[t]['data']['mu_pred'], dm.vars[t]['delta'].stats()['mean'])]
+                                              in zip(dm.vars[t]['data']['effective_sample_size'],
+                                                     dm.vars[t]['data']['value'],
+                                                     dm.vars[t]['data']['mu_pred'],
+                                                     pl.atleast_1d(dm.vars[t]['delta'].stats()['mean']))]
             dm.vars[t]['data'].to_csv(fname)
 
 
