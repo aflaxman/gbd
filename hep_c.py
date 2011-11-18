81ac6ff6c014615bd6d70b53430e7bfbe880f556
Abraham Flaxman <abie@alum.mit.edu>
2011-11-18 15:55:36 -0800

    extend hep c code to save results and compare them to baseline model

--- hep_c.py
+++ hep_c.py
@@ -37,7 +37,7 @@ def hold_out_quality():
     import glob
 
     results = pandas.DataFrame()
-    for fname in sorted(glob.glob('hep_c_figs/*.csv')):
+    for fname in sorted(glob.glob('hep_c_figs/sim-*.csv')):
         df = pandas.read_csv(fname)
         results = results.append(summarize(fname, df[df['effective_sample_size']==0]), ignore_index=True)
     results = results.reindex(columns='name bias mae pc'.split())
@@ -61,14 +61,49 @@ def store_fit(dm, key, est_k):
             pl.plot([0], [0], color='blue', linewidth=3, label='Old')
             pl.plot([0], [0], color='red', linewidth=3, label='New')
 
+
+    from dismod3 import neg_binom_model
+    t,r,y,s = dismod3.utils.type_region_year_sex_from_key(key)
+    if r == 'egy':
+        pop = neg_binom_model.population_by_age[('EGY', str(y), s)]
+    else:
+        pop = neg_binom_model.regional_population(key)
+
+    n = len(est_k)
+    cases = est_k*pop
+    total_cases = cases.sum(1)
+    total_cases.sort()
+    print 'total_cases %.0f (%.0f, %.0f)' % (total_cases.mean(), total_cases[.025*n], total_cases[.975*n])
+
     est_k.sort(axis=0)
     dm.set_mcmc('mean', key, pl.mean(est_k, axis=0))
     dm.set_mcmc('median', key, pl.median(est_k, axis=0))
-    n = len(est_k)
     dm.set_mcmc('lower_ui', key, est_k[.025*n,:])
     dm.set_mcmc('upper_ui', key, est_k[.975*n,:])
 
+    df = pandas.DataFrame(dict(pop=pop,
+                               prev=dm.get_mcmc('mean', key),
+                               prev_lb=dm.get_mcmc('lower_ui', key),
+                               prev_ub=dm.get_mcmc('upper_ui', key)),
+                          columns='pop prev prev_lb prev_ub'.split(),
+                          index=['age_%d'%i for i in range(101)])
+    df['cases'] = df['prev']*df['pop']
+    df['cases_lb'] = df['prev_lb']*df['pop']
+    df['cases_ub'] = df['prev_ub']*df['pop']
+
+    df_summary = pandas.DataFrame(dict(pop=df['pop'].sum(),
+                                       prev=df['cases'].sum()/df['pop'].sum(),
+                                       cases=total_cases.mean(),
+                                       cases_lb=total_cases[.025*n],
+                                       cases_ub=total_cases[.975*n]),
+                                  index=['all'],
+                                  columns=df.columns)
+    df = df_summary.append(df)
+    df.to_csv('hep_c_figs/cases-%s.csv'%key)
+
+
     dismod3.plotting.plot_mcmc_fit(dm, key, color='red')
+    pl.figtext(.2,.8, 'total_cases %.0f (%.1f, %.1f)' % (total_cases.mean(), total_cases[.025*n], total_cases[.975*n]))
     pl.title(key.replace('+', ', '))
 
     #graphics.plot_one_ppc(dm.vars, key.replace('+', ', '))
@@ -144,7 +179,7 @@ def hep_c_fit(regions, prediction_years, data_year_start=-pl.inf, data_year_end=
 
     dm.vars['data'] = dm.vars['data'].sort('logp')
     print dm.vars['data'].filter('area sex year_start age_start age_end effective_sample_size value mu_pred logp'.split())
-    dm.vars['data'].filter('area sex year_start age_start age_end effective_sample_size value mu_pred sigma_pred logp'.split()).to_csv('hep_c_figs/%s.csv'%'+'.join([str(x) for x in regions + prediction_years]))
+    dm.vars['data'].filter('area sex year_start age_start age_end effective_sample_size value mu_pred sigma_pred logp'.split()).to_csv('hep_c_figs/data-%s.csv'%'+'.join([str(x) for x in regions + prediction_years]))
 
 
     keys = dismod3.utils.gbd_keys(type_list=['prevalence'],
@@ -160,7 +195,7 @@ def hep_c_fit(regions, prediction_years, data_year_start=-pl.inf, data_year_end=
         store_fit(dm, key, est_k)
     print keys
     sim_and_fit(dm, keys)
- 
+
     return dm
 
 def sim_and_fit(dm, keys):
@@ -199,22 +234,22 @@ def sim_and_fit(dm, keys):
         print 'mae', pl.median(pl.absolute(results['residual'])),
         print 'pc', pl.mean(pl.absolute(results['residual']) < 1.96*results['sigma_pred'])
 
-        results.to_csv('hep_c_figs/sim_%s.csv'%key)
+        results.to_csv('hep_c_figs/sim-%s.csv'%key)
 
         store_fit(dm, 'sim_' + key, est_k)
 
 
 if __name__ == '__main__':
 
-    dm = hep_c_fit('caribbean latin_america_tropical latin_america_andean latin_america_central latin_america_southern'.split(), [1990, 2005])
-    dm = hep_c_fit('sub-saharan_africa_central sub-saharan_africa_southern sub-saharan_africa_west'.split(), [1990, 2005])
+    # dm = hep_c_fit('caribbean latin_america_tropical latin_america_andean latin_america_central latin_america_southern'.split(), [1990, 2005])
+    # dm = hep_c_fit('sub-saharan_africa_central sub-saharan_africa_southern sub-saharan_africa_west'.split(), [1990, 2005])
     
-    for r in 'europe_eastern europe_central asia_central asia_east asia_south asia_southeast australasia oceania sub-saharan_africa_east asia_pacific_high_income'.split():
-        dm = hep_c_fit([r], [1990, 2005])
+    # for r in 'europe_eastern europe_central asia_central asia_east asia_south asia_southeast australasia oceania sub-saharan_africa_east asia_pacific_high_income'.split():
+    #     dm = hep_c_fit([r], [1990, 2005])
 
-    for r in 'europe_western north_america_high_income'.split():
-        dm = hep_c_fit([r], [1990], data_year_end=1997)
-        dm = hep_c_fit([r], [2005], data_year_start=1997)
+    # for r in 'europe_western north_america_high_income'.split():
+    #     dm = hep_c_fit([r], [1990], data_year_end=1997)
+    #     dm = hep_c_fit([r], [2005], data_year_start=1997)
 
     dm_egypt = hep_c_fit(['EGY'], [1990, 2005], egypt_flag=True)
     dm_na_me = hep_c_fit(['north_africa_middle_east'], [1990, 2005])
@@ -245,3 +280,40 @@ if __name__ == '__main__':
 
             # save results
             #dismod3.post_disease_model(dm_na_me)
+
+
+    ## summarize results
+    import glob
+    csvs={}
+    for fname in glob.glob('hep_c_figs/cases-prev*.csv'):
+        csvs[fname] = pandas.read_csv(fname)
+
+    data = []
+    for k in csvs:
+        data.append(k.replace('.csv','').split('+')[1:] + list(csvs[k].ix[0,-3:]))
+
+    df = pandas.DataFrame(data, columns=['area', 'year', 'sex', 'cases', 'lb', 'ub'])
+
+    print 'Estimated cases (Millions):'
+    print (df.groupby('year').sum()/1000.)['cases'].round()  # TODO: estimate upperbound and lowerbound correctly
+
+    df.to_csv('hep_c_figs/cases.csv')
+
+    # compare to model 0 case estimates
+    df0 = pandas.read_csv('hep_c_figs/model_0_cases.csv')
+    
+
+    # TODO: patch pandas so that numeric data in year is still acceptible for groupby/aritmetic
+    df0['year'] = [str(y) for y in df0['year']]
+    df['year'] = [str(y) for y in df['year']]
+    delta = pl.absolute(df0.groupby(['area', 'year', 'sex']).mean()['cases'] - df.groupby(['area', 'year', 'sex']).mean()['cases'])
+    delta.sort()
+
+    print 'Global difference (Millions):'
+    print round(delta.sum()/1000,3)
+
+    print 'Maximum regional difference (Millions):'
+    print round(delta[-1]/1000,3)
+
+    print 'Median regional difference: (Millions):'
+    print round(pl.median(delta),3)
