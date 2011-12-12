b785451b77502f443e39aeb1fdf4754f4f361e3d
Abraham Flaxman <abie@alum.mit.edu>
2011-12-12 06:29:25 -0800

    vzv figure tweaks

--- book/vzv.py
+++ book/vzv.py
@@ -21,7 +21,7 @@ df = df[df['Year End']>=1997]
 ### @export 'plot-data'
 df = pandas.read_csv('vzv.csv', index_col=None)
 df = df[df['Region']=='North Africa/Middle East']
-df = df[df['Year End']>=1997]
+df = df[(df['Year End']>=1997) & (df['Year End']<=2005)]
 
 data_bars = zip(df['Age Start'], df['Age End']+1, df['Parameter Value'])
 
@@ -33,7 +33,7 @@ for a_0i, a_1i, p_i in data_bars:
     x += [a_0i, a_1i, pl.nan]
     y += [p_i, p_i, pl.nan]
 
-pl.plot(x, y, 'ks-', mew=1, mec='w', ms=4)
+pl.plot(x, y, 'ks-', mew=1, mec='w', ms=6, linewidth=2)
 pl.axis([-5, 55, 0, 1])
 pl.xlabel('Age (Years)')
 pl.ylabel('VZV Seroprevalence (Per 1)')
@@ -43,7 +43,7 @@ pl.savefig('vzv_data.pdf')
 ### @export 'forest-plot-age-5'
 df = pandas.read_csv('vzv.csv', index_col=None)
 df = df[df['Region']=='Europe, Western']
-df = df[df['Year End']>=1997]
+df = df[(df['Year End']>=1997) & (df['Year End']<=2005)]
 
 df5 = df[(df['Age Start'] <= 5) & (df['Age End'] >= 5)]
 r = df5['Parameter Value']
