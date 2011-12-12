9d5b209231e99572c6a7a5ac282304b42ed8a3dc
A. Flaxman <abie@alum.mit.edu>
2011-12-11 23:03:51 -0800

    add script to calculate median absolute relative error

---
+++ mare.py
@@ -0,0 +1,34 @@
+""" Find Median Absolute Relative Error of a DisMod model
+Example
+-------
+
+$ ./run_on_cluster.sh mare.py 27944
+
+"""
+
+import optparse
+import pylab as pl
+
+import upload_fits
+
+if __name__ == '__main__':
+    usage = 'usage: %prog [options] disease_model_id'
+    parser = optparse.OptionParser(usage)
+    (options, args) = parser.parse_args()
+
+    if len(args) != 1:
+        parser.error('incorrect number of arguments')
+
+    try:
+        id = int(args[0])
+    except ValueError:
+        parser.error('disease_model_id must be an integer')
+
+    df = upload_fits.merge_data_csvs(id)
+    df['are'] = pl.absolute((df['value'] - df['mu_pred']) / df['value'])
+
+    print 'mare by area:'
+    print pl.sort(df.groupby('area')['are'].median())
+    
+    print
+    print 'overall mare: %.3f' % (pl.median(df['are'].__array__()))
