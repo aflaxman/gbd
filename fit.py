d12e8cf29690916a88365269b3398614c5d59ec8
Abraham Flaxman <abie@alum.mit.edu>
2012-01-19 18:32:07 -0800

    fit_asr creates the model vars itself

--- fit.py
+++ fit.py
@@ -1,9 +1,11 @@
 """ Stub module for planned refactoring of dismod3 model fitting methods"""
 
 # TODO: move fit_model.fit_data_model to fit.fit_asr
+import dismod3
 import fit_model
 reload(fit_model)
 def fit_asr(model, data_type, iter=20000, burn=10000, thin=10, tune_interval=1000, verbose=True):
+    model.vars += dismod3.ism.age_specific_rate(model, data_type)
     model.map, model.mcmc = fit_model.fit_data_model(model.vars[data_type], iter, burn, thin, tune_interval, verbose)
 
 # TODO: move fit_model.fit_consistent_model to fit.fit_consistent
