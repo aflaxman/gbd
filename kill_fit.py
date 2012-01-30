0e084c37d124b0a61e9112ae598617e262b6efc8
A. Flaxman <abie@alum.mit.edu>
2012-01-29 19:42:57 -0800

    script for removing jobs from cluster

---
+++ kill_fit.py
@@ -0,0 +1,30 @@
+""" Remove all jobs from cluster
+
+Example
+-------
+
+$ python kill_fit.py 4222
+
+"""
+
+import optparse
+import subprocess
+
+if __name__ == '__main__':
+    usage = 'usage: %prog [options] disease_model_id'
+    parser = optparse.OptionParser(usage)
+    (options, args) = parser.parse_args()
+
+    if len(args) != 1:
+        parser.error('incorrect number of arguments')
+
+    try:
+        id = int(args[0])
+    except ValueError:
+        parser.error('disease_model_id must be an integer')
+
+    call_str = 'qstat | grep %s | awk {\'print $1\'} | xargs qdel' % id
+    print call_str
+    subprocess.call(call_str, shell=True)
+
+
