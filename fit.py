9ab511a25d4944b3772c9a4d76dcdd0be4867893
A. Flaxman <abie@alum.mit.edu>
2012-02-02 22:05:16 -0800

    enhancements to aid interactive notebook work

--- fit.py
+++ fit.py
@@ -4,11 +4,11 @@
 import dismod3
 import fit_model
 reload(fit_model)
-def fit_asr(model, data_type, iter=20000, burn=10000, thin=10, tune_interval=1000, verbose=True):
+def fit_asr(model, data_type, iter=2000, burn=1000, thin=1, tune_interval=100, verbose=False):
     model.map, model.mcmc = fit_model.fit_data_model(model.vars[data_type], iter, burn, thin, tune_interval, verbose)
 
 # TODO: move fit_model.fit_consistent_model to fit.fit_consistent
 import fit_model
-def fit_consistent(model, iter=20000, burn=10000, thin=10, tune_interval=1000, verbose=True):
+def fit_consistent(model, iter=2000, burn=1000, thin=1, tune_interval=100, verbose=False):
     model.map, model.mcmc = fit_model.fit_consistent_model(model.vars, iter, burn, thin, tune_interval, verbose)
 
