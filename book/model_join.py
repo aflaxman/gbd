71e7199713bc7b8ae40fab73e6c83559e6cab7e2
Hannah Peterson <peterhm@uw.edu>
2012-08-08 13:35:53 -0700

    all works

---
+++ book/model_join.py
@@ -0,0 +1,38 @@
+import pylab as pl
+import pandas
+import sys
+sys.path += ['.', '..', '/homes/peterhm/gbd/', '/homes/peterhm/gbd/book'] 
+
+replicates = int(sys.argv[1])
+
+# opens list of all models and rate types used
+model_list = pandas.read_csv('/clustertmp/dismod/model_info.csv')
+model_list = pl.array(model_list['model_list'], dtype='i')
+rate_types = pandas.read_csv('/clustertmp/dismod/model_types.csv')
+rate_types = pl.array(rate_types['rate_types'])
+
+# mean results from each model
+results = pandas.DataFrame(pl.zeros((len(model_list),6*len(rate_types)+1)), index = model_list)
+
+for num,m in enumerate(model_list):
+    output = []
+    for r,rate in enumerate(rate_types):
+        for i in range(replicates):
+            tmp = pandas.read_csv('/clustertmp/dismod/model_comparison_' + str(m) + rate + str(i) + '.csv')
+            # list of dataframes where each dataframe contains the replicates for a rate type
+            if i == 0:
+                output.append(tmp)
+            # and add to that dataframe
+            else:
+                output[r] = output[r].append(tmp, ignore_index=True)
+        # combine all rate_type dataframes of model to be saved
+        if r == 0:
+            output_save = output[0]
+        else:
+            output_save = output_save.join(output[r].drop(['seed'],axis=1))
+    # save all info for one model
+    output_save.to_csv('/homes/peterhm/gbd/book/model_' + str(m) + '.csv')
+    # report summary for model
+    if num == 0: results.columns = output_save.columns
+    results.ix[m,:] = output_save.mean()
+results.to_csv('/homes/peterhm/gbd/book/models.csv')    
