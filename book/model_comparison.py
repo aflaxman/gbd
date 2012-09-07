9036150f5466f7d17dd5063bcfdb401662bac36d
Hannah Peterson <peterhm@uw.edu>
2012-09-07 16:32:51 -0700

    model_comparison refactored, runs for log_normal with 0s

--- book/model_comparison.py
+++ book/model_comparison.py
@@ -30,11 +30,18 @@ failure = []
 
 # load new model
 model = mu.load_new_model(model_num, area, data_type)
+
+if rate_type == 'log_normal'
+    # find indices where values are 0
+    ix = mu.find_all(list(model.input_data['value']), 0)
+    # add 1 observation so no values are zero, also change effective sample size
+    model.input_data['effective_sample_size'][ix] = model.input_data['effective_sample_size'][ix] + 1
+    model.input_data['value'][ix] = 1.0/model.input_data['effective_sample_size'][ix]
+    
 # withhold 25% of data
 model, test_ix = mu.test_train(model, data_type, replicate)
 
 try:
-    fail = 0
     # create pymc nodes for model and fit the model
     model = mu.create_new_vars(model, rate_type, data_type, area, 'male', 2005, iter, thin, burn)
     # fit the model, using a hill-climbing alg to find an initial value
@@ -42,38 +49,7 @@ try:
     start = time.clock()
     dismod3.fit.fit_asr(model, data_type, iter=iter, thin=thin, burn=burn)
     elapsed = (time.clock() - start)
-except:
-    print 'exception'
-    try:
-        print 'trying again'
-        # load new model (if value ==0, want to change value and effective sample size)
-        model = mu.load_new_model(model_num, area, data_type)
-        
-        # find indices where values are 0
-        ix = mu.find_all(list(model.input_data['value']), 0)
-        # add 1 observation so no values are zero, also change effective sample size
-        model.input_data['effective_sample_size'][ix] = model.input_data['effective_sample_size'][ix] + 1
-        model.input_data['value'][ix] = 1.0/model.input_data['effective_sample_size'][ix]
-        
-        # withhold 25% of data
-        model, test_ix = mu.test_train(model, data_type, replicate)
-        
-        # create pymc nodes for model and fit the model
-        model = mu.create_new_vars(model, rate_type, data_type, area, 'male', 2005, iter, thin, burn)
-        # fit the model, using a hill-climbing alg to find an initial value
-        # and then sampling from the posterior with MCMC
-        start = time.clock()
-        dismod3.fit.fit_asr(model, data_type, iter=iter, thin=thin, burn=burn)
-        elapsed = (time.clock() - start)
-    except:
-        print 'fail'
-        fail = 1
-        # want to know which models fail 
-        failure.append((model_num, rate_type, replicate))
-        failure = pandas.DataFrame(failure, columns=['model', 'rate_type', 'replicate'])
-        failure.to_csv('/clustertmp/dismod/model_failure_' + str(model_num) + rate_type + str(replicate) + '.csv')
-
-if fail != 1:      
+    
     # extract posterior predicted values for data
     pred = pandas.DataFrame(model.vars[data_type]['p_pred'].stats()['mean'], columns=['mean'], index=model.input_data.index)
     pred_ui = pandas.DataFrame(model.vars[data_type]['p_pred'].stats()['95% HPD interval'], columns=['lower', 'upper'], index=model.input_data.index) 
@@ -84,7 +60,7 @@ if fail != 1:
     obs_test = obs.ix[test_ix]
     pred_ui_test = pred_ui.ix[test_ix]
 
-    # methods of comparison
+    # record statistics for test data
     output['bias_'+rate_type] = mu.bias(pred_test, obs_test)
     output['rmse_'+rate_type] = mu.rmse(pred_test, obs_test)
     output['mae_'+rate_type] = mu.mae(pred_test, obs_test)
@@ -105,3 +81,12 @@ if fail != 1:
     if replicate == 0:
         model_stats = pandas.DataFrame(['seed', 'bias_', 'rmse_', 'mae_', 'mare_', 'pc_', 'time_'], columns=['stats'])
         model_stats.to_csv('/homes/peterhm/gbd/book/validity/model_stats.csv')
+    
+except Exception, e:
+    print 'exception'
+    print e
+    # want to know which models fail 
+    failure.append((model_num, rate_type, replicate))
+    failure = pandas.DataFrame(failure, columns=['model', 'rate_type', 'replicate'])
+    failure.to_csv('/clustertmp/dismod/model_failure_' + str(model_num) + rate_type + str(replicate) + '.csv')
+    
