2534abadf15fefe18efc55a1b293ba6eb5446840
Hannah Peterson <peterhm@uw.edu>
2013-01-12 20:49:32 -0800

    book: edits to fruit figs, horrible

--- book/graphics_fruit_isl_rt_type.py
+++ book/graphics_fruit_isl_rt_type.py
@@ -20,30 +20,6 @@ book_graphics.set_font()
 def my_axis(ymax):
     pl.axis([-5,105,-ymax/10.,ymax])
     
-def my_plot_data_bars(df, color, label, style='book'):
-    """ Plot some data bars
-    Input
-    -----
-    df : pandas.DataFrame with columns age_start, age_end, value
-    """
-    data_bars = zip(df['age_start'], df['age_end'], df['value'])
-
-    # show at most 500 bars, to keep things fast
-    # TODO: make 500 into an option
-    if len(data_bars) > 500:
-        import random
-        data_bars = random.sample(data_bars, 500)
-
-    # make lists of x and y points, faster than ploting each bar
-    # individually
-    x = []
-    y = []
-    for a_0i, a_1i, p_i in data_bars:
-        x += [a_0i, a_1i, pl.nan]
-        y += [p_i, p_i, pl.nan]
-
-    pl.plot(x, y, 's-', mew=1, mec='w', ms=4, color=color, label=label)    
-    
 # load data to plot
 age_pred = pandas.read_csv('/home/j/Project/dismod/gbd/data/applications-data_fruit_age_pred.csv', index_col=0)
 ui_pred = pandas.read_csv('/home/j/Project/dismod/gbd/data/applications-data_fruit_ui_pred.csv', index_col=0)
@@ -56,36 +32,50 @@ labeling = dict(we_model=dict(style='k-', lab='Negative-binomial '),
                 we_log_model=dict(style='k--', lab='Lognormal '), 
                 we_norm_model=dict(style='k:', lab='Normal '))
 
-pl.figure(**book_graphics.full_page_params)
+fig = pl.figure(**book_graphics.three_quarter_page_params)
 knots = list(ui_pred.index)
-k=0
-for i in ['ISL', 'GRC']:
-    k=k+1
-    pl.subplot(1,2,k)
-    my_plot_data_bars(we, color='grey', label='Western Europe') 
-    if i == 'ISL': my_plot_data_bars(isl, color='black', label='Iceland')
-    elif i == 'GRC': my_plot_data_bars(grc, color='black', label='Greece')
 
-    for m in ['we_model', 'we_log_model', 'we_norm_model']:
-        pred = pl.array(age_pred[m + '_' + i])
-        we_hpd_l = pl.array(ui_pred[m + '_' + i + '_l'])
-        we_hpd_u = pl.array(ui_pred[m + '_' + i + '_u'])
+# plot 1
+ax1 = fig.add_subplot(1,2,1)
+ax1.plot(dismod3.graphics.plot_data_bars(we, color='grey', label='Western Europe') )
+ax1.plot(dismod3.graphics.plot_data_bars(isl, color='black', label='Iceland'))
+ax1.plot(dismod3.graphics.plot_data_bars(grc, color='black', label='Greece'))
+pl.xlabel('Age (years)')
+pl.ylabel('Consumption (kg/d)')
+pl.yticks([0, .015, .03, .045, .06], [0, 0.15, 0.30, 0.45, 0.6])
+my_axis(.075)
+# plot 2, 3
 
-        pl.plot(pred, labeling[m]['style'], linewidth=3, label=labeling[m]['lab']+'posterior mean')
-        pl.plot(knots, we_hpd_l, labeling[m]['style'], linewidth=1, label=labeling[m]['lab']+'95% HPD interval')
-        pl.plot(knots, we_hpd_u, labeling[m]['style'], linewidth=1)
 
-    pl.xlabel('Age (years)')
-    pl.ylabel('Consumption (kg/d)')
-    pl.yticks([0, .015, .03, .045, .06], [0, 0.15, 0.30, 0.45, 0.6])
-    my_axis(.075)
-    if i == 'ISL': book_graphics.subtitle('(a)')
-    elif i == 'GRC': book_graphics.subtitle('(b)')
-    pl.legend(loc='upper center', bbox_to_anchor=(.5,-.33), fancybox=True, shadow=True)
     
+for i in ['GRC', 'ISL']: #in ['we_model', 'we_log_model', 'we_norm_model']:
+    pred = pl.array(age_pred['we_model_' + i])
+    we_hpd_l = pl.array(ui_pred['we_model_' + i + '_l'])
+    we_hpd_u = pl.array(ui_pred['we_model_' + i + '_u'])
+    # plot 1
+    ax1.plot(pred, labeling['we_model']['style'], linewidth=3, label=labeling['we_model']['lab']+'posterior mean')
+    ax1.plot(knots, we_hpd_l, labeling['we_model']['style'], linewidth=1, label=labeling['we_model']['lab']+'95% HPD interval')
+    ax1.plot(knots, we_hpd_u, labeling['we_model']['style'], linewidth=1)  
+    for m in ['we_log_model', 'we_norm_model']:
+        m_pred = pl.array(age_pred[m + '_' + i])
+        if i == 'GRC': 
+            ax2 = fig.add_subplot(2,2,2)
+            ax2.plot((pred - m_pred), labeling[m]['style'], linewidth=3, label='Negative-binomial - ' + labeling[m]['lab'])
+            pl.axis([-5, 105, -.007, .017])
+            pl.yticks([-.005, -.0, .005, .01, .015])
+            pl.ylabel('Greece')
+        elif i == 'ISL': 
+            ax3 = fig.add_subplot(2,2,4, sharex=ax2)
+            ax3.plot((pred - m_pred), labeling[m]['style'], linewidth=3, label='Negative-binomial - ' + labeling[m]['lab'])
+            pl.axis([-5, 105, -.0022, .0008])
+            pl.yticks([-.002, -.0015, -.001, -.0005, .0, .0005])
+            pl.xlabel('Age (years)')
+            pl.ylabel('Iceland')
+        
+pl.setp(ax2.get_xticklabels(), visible=False)
+pl.legend(loc='upper center', bbox_to_anchor=(-.2,-.33), fancybox=True, shadow=True, ncol=3)
     
-pl.subplots_adjust(top=.93, bottom=.53, wspace=.35)
-
+pl.subplots_adjust(top=.99, bottom=.27, wspace=.45, hspace=.1)
 pl.savefig('book/graphics/fruit-isl_rate_type.pdf')
 pl.savefig('book/graphics/fruit-isl_rate_type.png')
 
--- book/graphics_fruit_rt_we_rt.py
+++ book/graphics_fruit_rt_we_rt.py
@@ -16,31 +16,7 @@ book_graphics.set_font()
 
 def my_axis(ymax):
     pl.axis([-5,105,-ymax/10.,ymax])
-	
-def my_plot_data_bars(df, color, label, style='book'):
-    """ Plot some data bars
-    Input
-    -----
-    df : pandas.DataFrame with columns age_start, age_end, value
-    """
-    data_bars = zip(df['age_start'], df['age_end'], df['value'])
-
-    # show at most 500 bars, to keep things fast
-    # TODO: make 500 into an option
-    if len(data_bars) > 500:
-        import random
-        data_bars = random.sample(data_bars, 500)
-
-    # make lists of x and y points, faster than ploting each bar
-    # individually
-    x = []
-    y = []
-    for a_0i, a_1i, p_i in data_bars:
-        x += [a_0i, a_1i, pl.nan]
-        y += [p_i, p_i, pl.nan]
-
-    pl.plot(x, y, 's-', mew=1, mec='w', ms=4, color=color, label=label)
-    
+   
 def load_USA_new_model():
     # example of predicting out-of-sample with a ln_ASDR covariate
     try:
@@ -69,6 +45,7 @@ output = pandas.read_csv('/home/j/Project/dismod/gbd/data/applications-data_frui
 # figure 
 
 pl.figure(**book_graphics.full_page_params)
+pl.subplot(1,2,1)
 dismod3.graphics.plot_data_bars(best_model.get_data('r'), color='grey') 
    
 knots = best_model.parameters['r']['parameter_age_mesh']
@@ -89,44 +66,18 @@ pl.xlabel('Age (years)')
 pl.ylabel('Consumption (kg/d)')
 pl.yticks([0, .006, .012, .018, .024], [0, 0.06, 0.12, 0.18, 0.24])
 my_axis(.026)
-pl.legend(loc='lower right', fancybox=True, shadow=True)
+pl.legend(loc='upper center', bbox_to_anchor=(.2,-.23), fancybox=True, shadow=True) 
 
+pl.subplot(1,2,2)
+pl.plot(pl.array(output['USA_model'])-pl.array(output['USA_log_model']), 'k--', linewidth=3, label='Negative-binomial -\n lognormal posterior mean')
+pl.plot(pl.array(output['USA_model'])-pl.array(output['USA_norm_model']), 'k:', linewidth=3, label='Negative-binomial -\n normal posterior mean')
+
+pl.yticks([-.0001, -.0, .0001, .0002])
+pl.axis([-5,105,-.00015,.00025])
+
+pl.subplots_adjust(wspace=.5, top=.99, bottom=.62)
+pl.legend(loc='upper center', bbox_to_anchor=(.2,-.23), fancybox=True, shadow=True) 
 
 pl.savefig('book/graphics/fruit-rate_type.pdf')
 pl.savefig('book/graphics/fruit-rate_type.png')
 
-# figure-we_rate_type
-isl_gbr = load_we_new_model()
-isl_gbr.keep(areas=['ISL', 'GBR'])
-
-pl.figure(**book_graphics.full_page_params)
-my_plot_data_bars(we_model.get_data('r'), color='grey', label='Western Europe') 
-my_plot_data_bars(isl_gbr.get_data('r'), color='black', label='Great Britain and Iceland')
-
-knots = we_model.parameters['r']['parameter_age_mesh']
-
-pl.plot(pl.array(output['we_model']), 'k-', linewidth=3, label='Negative-binomial posterior mean')
-pl.plot(knots, pl.array(output['we_model_l'])[knots], 'k-', linewidth=1, label='Negative-binomial 95% HPD interval')
-pl.plot(knots, pl.array(output['we_model_u'])[knots], 'k-', linewidth=1)
-
-pl.plot(pl.array(output['we_log_model']), 'k--', linewidth=3, label='Lognormal posterior mean')
-pl.plot(knots, pl.array(output['we_log_model_l'])[knots], 'k--', linewidth=1, label='Lognormal 95% HPD interval')
-pl.plot(knots, pl.array(output['we_log_model_u'])[knots], 'k--', linewidth=1)
-
-pl.plot(pl.array(output['we_norm_model']), 'k:', linewidth=3, label='Normal posterior mean')
-pl.plot(knots, pl.array(output['we_norm_model_l'])[knots], 'k:', linewidth=1, label='Normal 95% HPD interval')
-pl.plot(knots, pl.array(output['we_norm_model_u'])[knots], 'k:', linewidth=1)
-
-pl.xlabel('Age (years)')
-pl.ylabel('Consumption (kg/d)')
-pl.yticks([0, .01, .02, .03, .04], [0, 0.1, 0.2, 0.3, 0.4])
-my_axis(.06)
-pl.legend(loc='upper right', fancybox=True, shadow=True)
-
-
-pl.subplots_adjust(hspace=.3)
-
-pl.savefig('book/graphics/fruit-we_rate_type.pdf')
-pl.savefig('book/graphics/fruit-we_rate_type.png')
-
-pl.show()
